c3d5089caa539aca877e04854cdfd19fc70fb29f
Francesco Balestrieri <francesco.balestrieri@intel.com>
2017-09-26 19:24:13 +0300

    Preprocessing functions are now in a separate module

---
+++ preprocessing.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+def process_data(data):
+    add_title(data)
+    add_deck(data)
+    process_age(data)
+    process_fare(data)
+    process_embarked(data)
+    process_sibsp(data)
+    process_parch(data)
+
+    data.drop(['Name', 'PassengerId', 'Ticket', 'Cabin'], axis=1, inplace=True)
+    data = pd.get_dummies(data, columns=['Pclass', 'Sex', 'Embarked', 'Title', 'Deck'])
+
+    X_train = data.ix['train'].values
+    X_test = data.ix['test'].values
+
+    return X_train, X_test
+
+def add_title(data):
+    data['Title'] = data['Name'].apply(lambda x: x.split(',')[1].split('.')[0].strip())
+
+def add_deck(data):
+    data['Deck'] = data['Cabin'].apply(lambda x: str(x)[0])
+    data['Deck'].fillna('U', inplace = True)
+
+def process_age(data):
+    # Fill in missing age with the average on a given title/sex
+    age_by_title = data.groupby(['Title'])['Age'].mean()
+    data['Age'] = data.apply(
+        lambda row:
+            age_by_title[row['Title']] if np.isnan(row['Age'])
+            else row['Age'], axis=1
+        )
+
+    data['Age'] = StandardScaler().fit_transform(data['Age'].values.reshape(-1, 1))
+
+def process_fare(data):
+    fare_by_class = data.groupby(['Pclass'])['Fare'].mean()
+    data['Fare'] = data.apply(
+        lambda row:
+            fare_by_class[row['Pclass']] if np.isnan(row['Fare'])
+            else row['Fare'], axis=1
+        )
+
+    data['Fare'] = StandardScaler().fit_transform(data['Fare'].values.reshape(-1, 1))
+
+def process_embarked(data):
+    most_frequent_port = data['Embarked'].value_counts().idxmax()
+    data['Embarked'].fillna(most_frequent_port, inplace = True)
+
+def process_sibsp(data):
+    data['SibSp'] = StandardScaler().fit_transform(data['SibSp'].values.reshape(-1, 1))
+
+def process_parch(data):
+    data['Parch'] = StandardScaler().fit_transform(data['Parch'].values.reshape(-1, 1))
--- titanic.py
+++ titanic.py
@@ -7,7 +7,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.linear_model import LogisticRegressionCV
-from sklearn import preprocessing
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
@@ -18,45 +17,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.externals import joblib
 
-def add_title(data):
-    data['Title'] = data['Name'].apply(lambda x: x.split(',')[1].split('.')[0].strip())
+import preprocessing as pp# Local module
 
-def add_deck(data):
-    data['Deck'] = data['Cabin'].apply(lambda x: str(x)[0])
-    data['Deck'].fillna('U', inplace = True)
-
-def process_age(data):
-    # Fill in missing age with the average on a given title/sex
-    age_by_title = data.groupby(['Title'])['Age'].mean()
-    data['Age'] = data.apply(
-        lambda row:
-            age_by_title[row['Title']] if np.isnan(row['Age'])
-            else row['Age'], axis=1
-        )
-
-    data['Age'] = StandardScaler().fit_transform(data['Age'].values.reshape(-1, 1))
-
-def process_fare(data):
-    fare_by_class = data.groupby(['Pclass'])['Fare'].mean()
-    data['Fare'] = data.apply(
-        lambda row:
-            fare_by_class[row['Pclass']] if np.isnan(row['Fare'])
-            else row['Fare'], axis=1
-        )
-
-    data['Fare'] = StandardScaler().fit_transform(data['Fare'].values.reshape(-1, 1))
-
-def process_embarked(data):
-    most_frequent_port = data['Embarked'].value_counts().idxmax()
-    data['Embarked'].fillna(most_frequent_port, inplace = True)
-
-def process_sibsp(data):
-    data['SibSp'] = StandardScaler().fit_transform(data['SibSp'].values.reshape(-1, 1))
-
-def process_parch(data):
-    data['Parch'] = StandardScaler().fit_transform(data['Parch'].values.reshape(-1, 1))
-
-# Pick the selected features and apply the transformations above.
+# Pick the selected features and apply the preprocessing transformations.
 # Return the feature matrix, target vector (if present) and passenger IDs.
 # Works on both training and testing data.
 def get_data(traindatafile, testdatafile):
@@ -67,19 +30,8 @@ def get_data(traindatafile, testdatafile):
     test_data = pd.read_csv(testdatafile)
     data = pd.concat([train_data, test_data], keys = ['train', 'test'])
 
-    add_title(data)
-    add_deck(data)
-    process_age(data)
-    process_fare(data)
-    process_embarked(data)
-    process_sibsp(data)
-    process_parch(data)
+    X_train, X_test = pp.process_data(data)
 
-    data.drop(['Name', 'PassengerId', 'Ticket', 'Cabin'], axis=1, inplace=True)
-    data = pd.get_dummies(data, columns=['Pclass', 'Sex', 'Embarked', 'Title', 'Deck'])
-
-    X_train = data.ix['train'].values
-    X_test = data.ix['test'].values
     pid_test = test_data['PassengerId']
 
     return X_train, X_test, pid_test, y
